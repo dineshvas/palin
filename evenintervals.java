7f350001b1069c5392b9746cc0d6b285cd9b120d
dineshvas <dineshkumar9569@gmail.com>
2017-06-19 12:25:40 +0530

    Add files via upload

---
+++ evenintervals.java
@@ -0,0 +1,12 @@
+package guvi;
+
+public class evenintervals {public static void main(String[] args){
+	{
+		int i;
+		for(i=1;i<=30;i++){
+		if(i%2==0)			{
+		System.out.println(i);
+		}
+		}
+	}}}
+
